d74adbd285970f1b522ee3ad4691c7693ed474c0
Kevin Julian <kevin.julian.372@gmail.com>
2024-09-18 21:31:24 -0500

    actualizacion js

--- main.js
+++ main.js
@@ -11,27 +11,26 @@ let contrasena = document.getElementById('contrasena');
 function generar() {
     let numeroDigitado = parseInt(cantidad.value);
     let password = '';
+    
 
     if (numeroDigitado < 8) {
         alert("La cantidad de caracteres debe ser mayor que 8");
     } else {
         alert('Contraseña creada correctamente');
+        let mayuscula = letrasMayusculas[Math.floor(Math.random() * letrasMayusculas.length)];
+        let minuscula = letrasMinusculas[Math.floor(Math.random() * letrasMinusculas.length)];
+        let numero = numeros[Math.floor(Math.random() * numeros.length)];
+        let especial = caracteresEspeciales[Math.floor(Math.random() * caracteresEspeciales.length)];
+        password += mayuscula + minuscula + numero + especial;
+        for (let i = password.length; i < numeroDigitado; i++) {
+            let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+            password += caracterAleatorio;
+        }
+    
+        password = password.split('').sort(() => Math.random() - 0.5).join('');
     }
-
-    let mayuscula = letrasMayusculas[Math.floor(Math.random() * letrasMayusculas.length)];
-    let minuscula = letrasMinusculas[Math.floor(Math.random() * letrasMinusculas.length)];
-    let numero = numeros[Math.floor(Math.random() * numeros.length)];
-    let especial = caracteresEspeciales[Math.floor(Math.random() * caracteresEspeciales.length)];
-
-    password += mayuscula + minuscula + numero + especial;
-
-    for (let i = password.length; i < numeroDigitado; i++) {
-        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
-        password += caracterAleatorio;
-    }
-
-    password = password.split('').sort(() => Math.random() - 0.5).join('');
     contrasena.value = password;
+
 }
 
 function reiniciar() {
